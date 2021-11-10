dff37b44e41791105d5c9bd43c34bf22feb04f17
delpot <delpot@users.noreply.github.com>
2021-11-10 15:15:25 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.73a06be9bf439a36136eff65d336bf02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42426656a745dc7b6dba730c1633699f",
+    "url": "https://delpot.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "5349008a9b3114089b62",
+    "url": "https://delpot.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c7c6254f26c44a5aa70e",
+    "url": "https://delpot.github.io/task-management-frontend/static/js/2.3fcff1b8.chunk.js"
+  },
+  {
+    "revision": "5349008a9b3114089b62",
+    "url": "https://delpot.github.io/task-management-frontend/static/js/main.8ca379fe.chunk.js"
+  },
+  {
+    "revision": "09a0cae6e31714a7b701",
+    "url": "https://delpot.github.io/task-management-frontend/static/js/runtime~main.351c37f5.js"
+  }
+]);
